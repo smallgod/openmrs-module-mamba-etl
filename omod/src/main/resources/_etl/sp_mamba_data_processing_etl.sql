d2f1cf7acd31015a123d5affdd9a60775980cf91
smallGod <davies.mugume@gmail.com>
2024-05-17 07:34:42 +0300

    support for the latest changes in core

--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -1,9 +1,11 @@
 -- $BEGIN
--- add base folder SP here --
 
--- Flatten OpenMRS Observational Data
+-- Flatten the tables first
 CALL sp_mamba_data_processing_flatten();
 
--- Add the implementation-specific ETL processes here
+CALL sp_mamba_data_processing_derived_hts();
+-- CALL sp_mamba_data_processing_derived_covid();
+CALL sp_mamba_data_processing_derived_pmtct();
 
+CALL sp_mamba_extract_report_column_names();
 -- $END
